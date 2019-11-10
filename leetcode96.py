24d6dc0af5ee95e7efd4ac66ee91f4b5833c5c0a
Julian Chu <yulang.chu@gmail.com>
2019-11-10 12:52:43 +0100

    add leetcode96

--- leetcode96.py
+++ leetcode96.py
@@ -3,20 +3,19 @@ from unittest import TestCase
 
 class Solution:
     def numTrees(self, n: int) -> int:
-        if n==0:
-            return 1
-        if n==1 or n==2:
+        if n<2:
             return n
-        if n==3:
-            return 5
-        sum=0
-        for i in range(1,n//2+1):
-            sum+= self.numTrees(i-1)*self.numTrees(n-i)
-        sum*=2
-        if n%2==1:
-            temp = self.numTrees(n//2)
-            sum+=temp*temp
-        return sum
+        if n==2:
+            return 2
+
+        arr = [0 for i in range(n+1)]
+        arr[0], arr[1] = 1,1
+
+        for i in range(2,n+1):
+           for j in range(1,i+1):
+            arr[i] += arr[j-1]*arr[i-j]
+
+        return arr[-1]
 
 
 class TestSolution(TestCase):
