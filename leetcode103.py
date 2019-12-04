1f5f5bdd3c5099946fb32c00c3f84ce10c3b6479
Julian Chu <yulang.chu@gmail.com>
2019-12-04 22:45:05 +0100

    add leetcode103

---
+++ leetcode103.py
@@ -0,0 +1,22 @@
+
+from helper import TreeNode
+
+class Solution:
+    def zigzagLevelOrder(self, root: TreeNode) -> List[List[int]]:
+        res = self.BuildLevelOrder(root, [], 0)
+        return res
+
+    def BuildLevelOrder(self, root, res: List[List[int]], level) -> List[List[int]]:
+        if not root:
+            return res
+
+        if len(res) == level:
+            res.append([])
+
+        if level%2==0:
+            res[level].append(root.val)
+        else:
+            res[level].insert(0,root.val)
+        res = self.BuildLevelOrder(root.left, res, level + 1)
+        res = self.BuildLevelOrder(root.right, res, level + 1)
+        return res
