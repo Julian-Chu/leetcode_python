2d0dbf7c339edee95b07439fac931d0f3796f30b
yu-lang.chu <yu-lang.chu@redheads.de>
2019-12-16 12:22:17 +0100

    add leetcode129

--- leetcode129.py
+++ leetcode129.py
@@ -2,20 +2,21 @@ from helper import TreeNode
 
 
 class Solution:
+    cur_sum = 0
+
     def sumNumbers(self, root: TreeNode) -> int:
-        return self.dfs(root, 0)
 
-    def dfs(self, root: TreeNode, sum: int):
-        if not root:
-            return sum
-        sum *= 10
-        sum += root.val
-        if not root.left and not root.right:
-            return sum
-        res = 0
-        if root.left:
-            res += self.dfs(root.left, sum)
-        if root.right:
-            res += self.dfs(root.right, sum)
+        def dfs(root, cur_val):
+            if not root:
+                return
+            cur_val = cur_val * 10 + root.val
 
-        return res
+            if not root.left and not root.right:
+                self.cur_sum += cur_val
+
+            dfs(root.left, cur_val)
+            dfs(root.right, cur_val)
+
+        dfs(root, 0)
+
+        return self.cur_sum
