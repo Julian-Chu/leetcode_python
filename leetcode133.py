5cfe7e7b8c0a1640e1835d8d0ac7f4e2999c2e9b
yu-lang.chu <yu-lang.chu@redheads.de>
2020-01-21 17:41:17 +0100

    add leetcode133(bfs)

--- leetcode133.py
+++ leetcode133.py
@@ -15,15 +15,29 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        def dfs(node: 'Node', dict):
-            if not node:
-                return None
-            if node  in dict:
-                return dict[node]
-            node_clone = Node(node.val)
-            dict[node] = node_clone
-            for n in node.neighbors:
-                node_clone.neighbors.append(dfs(n, dict))
-            return node_clone
+        if not node: return None
+        q = [node]
+        dict = {node: Node(node.val)}
+        while q:
+            t = q.pop(-1)
+            if not t: continue
+            for n in t.neighbors:
+                if n not in dict:
+                    dict[n] = Node(n.val)
+                    q.append(n)
+                dict[t].neighbors.append(dict[n])
+        return dict[node]
 
-        return dfs(node, {})
+    # def cloneGraph(self, node: 'Node') -> 'Node':
+    #     def dfs(node: 'Node', dict):
+    #         if not node:
+    #             return None
+    #         if node  in dict:
+    #             return dict[node]
+    #         node_clone = Node(node.val)
+    #         dict[node] = node_clone
+    #         for n in node.neighbors:
+    #             node_clone.neighbors.append(dfs(n, dict))
+    #         return node_clone
+    #
+    #     return dfs(node, {})
