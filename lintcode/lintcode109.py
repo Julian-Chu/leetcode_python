fa4a2db6e0de943d46dd74ebb78dc5fa0d6c8a62
Julian Chu <yulang.chu@gmail.com>
2020-12-13 18:00:50 +0100

    add lintcode by using dp and rolling array

--- lintcode/lintcode109.py
+++ lintcode/lintcode109.py
@@ -59,3 +59,59 @@ class Solution:
         right = self.divide_conquer(triangle, x + 1, y + 1, memo)
         memo[(x, y)] = triangle[x][y] + min(left, right)
         return memo[(x, y)]
+
+
+"""
+dp
+"""
+
+
+class Solution:
+    """
+    @param triangle: a list of lists of integers
+    @return: An integer, minimum path sum
+    """
+
+    def minimumTotal(self, triangle):
+        if not triangle:
+            return 0
+        n = len(triangle)
+        dp = [[0] * (i + 1) for i in range(n)]
+
+        dp[0][0] = triangle[0][0]
+
+        for i in range(1, n):
+            dp[i][0] = dp[i - 1][0] + triangle[i][0]
+            dp[i][i] = dp[i - 1][i - 1] + triangle[i][i]
+
+        for i in range(2, n):
+            for j in range(1, i):
+                dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1]) + triangle[i][j]
+
+        return min(dp[n - 1])
+
+"""
+dp + rolling array
+"""
+
+class Solution:
+    """
+    @param triangle: a list of lists of integers
+    @return: An integer, minimum path sum
+    """
+
+    def minimumTotal(self, triangle):
+        if not triangle:
+            return 0
+        n = len(triangle)
+        dp = [[0] * n, [0] * n]
+
+        dp[0][0] = triangle[0][0]
+
+        for i in range(1, n):
+            dp[i % 2][0] = dp[(i - 1) % 2][0] + triangle[i][0]
+            dp[i % 2][i] = dp[(i - 1) % 2][i - 1] + triangle[i][i]
+            for j in range(1, i):
+                dp[i % 2][j] = min(dp[(i - 1) % 2][j], dp[(i - 1) % 2][j - 1]) + triangle[i][j]
+
+        return min(dp[(n - 1) % 2])
