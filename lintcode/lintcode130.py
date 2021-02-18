1e9d801925fd2e3782ac781f7a824923678f5fe8
Julian Chu <yulang.chu@gmail.com>
2021-02-18 23:58:54 +0100

    add ds

--- lintcode/lintcode130.py
+++ lintcode/lintcode130.py
@@ -1,30 +1,9 @@
-class Solution:
-    """
-    @param: A: Given an integer array
-    @return: nothing
-    """
+"""
+sift down
 
-    def heapify(self, A):
-        for i in range(len(A) // 2, -1, -1):
-            self.siftdown(A, i)
-
-    def siftdown(self, A, i):
-        while i < len(A):
-            left = i * 2 + 1
-            right = i * 2 + 2
-            minIndex = i
-            if right < len(A) and A[right] < A[minIndex]:
-                minIndex = right
-            if left < len(A) and A[left] < A[minIndex]:
-                minIndex = left
-
-            if minIndex == i:
-                break
-
-            A[minIndex], A[i] = A[i], A[minIndex]
-            i = minIndex
-
-
+O(n) 是因为从第 N/2 个位置开始往下 siftdown，那么就有大约 N/4 个数在 siftdown 中最多交换 1 次，N/8 个数最多交换 2 次，N/16 个数最多交换 3 次。 所以
+O(N/4∗1+N/8∗2+N/16∗3+...+1∗LogN)=O(N)
+"""
 class Solution:
     """
     @param: A: Given an integer array
@@ -45,3 +24,27 @@ class Solution:
                 break
             A[son], A[i] = A[i], A[son]
             i = son
+
+"""
+sift up
+O(nlogn)
+"""
+
+class Solution:
+    """
+    @param: A: Given an integer array
+    @return: nothing
+    """
+
+    def heapify(self, A):
+        for i in range(len(A)):
+            self.siftup(A, i)
+
+    def siftup(self, A, i):
+        if i == 0:
+            return
+        parentIdx = (i - 1) // 2
+        while i > 0 and A[i] < A[parentIdx]:
+            A[i], A[parentIdx] = A[parentIdx], A[i]
+            i = parentIdx
+            parentIdx = (i - 1) // 2
