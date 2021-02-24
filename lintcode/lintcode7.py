8dae79b0af54d7cd1448d47ea7780d844edf24f8
yu-lang.chu <yu-lang.chu@redheads.de>
2021-02-24 09:52:15 +0100

    add serialize and deserialize eg

--- lintcode/lintcode7.py
+++ lintcode/lintcode7.py
@@ -60,17 +60,18 @@ class Solution:
     """
 
     def serialize(self, root):
-        if root is None:
-            return ""
-
-        queue = deque([root])
+        queue = collections.deque([root])
         bfs_order = []
         while queue:
             node = queue.popleft()
-            bfs_order.append(str(node.val) if node else '#')
-            if node:
-                queue.append(node.left)
-                queue.append(node.right)
+            if node is None:
+                bfs_order.append('#')
+                continue
+
+            bfs_order.append(str(node.val))
+            queue.append(node.left)
+            queue.append(node.right)
+
         return ' '.join(bfs_order)
 
     """
@@ -83,24 +84,18 @@ class Solution:
     """
 
     def deserialize(self, data):
-        print(data)
         if not data:
-            return None
-
-        bfs_order = [TreeNode(int(val)) if val != '#' else None for val in data.split()]
-        root = bfs_order[0]
-        fast_index = 1
-
-        nodes, slow_index = [root], 0
-        while slow_index < len(nodes):
-            node = nodes[slow_index]
-            slow_index += 1
-            node.left = bfs_order[fast_index]
-            node.right = bfs_order[fast_index + 1]
-            fast_index += 2
-
-            if node.left:
-                nodes.append(node.left)
-            if node.right:
-                nodes.append(node.right)
+            return  None
+        data = data.split()
+        res = [TreeNode(int(string)) if string != '#' else None for string in data]
+        root = res[0]
+        slow, fast = 0, 1
+        while slow < len(data):
+            # while fast < len(data):
+            node = res[slow]
+            slow += 1
+            if node:
+                node.left = res[fast]
+                node.right = res[fast + 1]
+                fast += 2
         return root
