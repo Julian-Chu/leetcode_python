8fab3fc4ebe8928cbc222297fcf71cf75df4a1d4
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-01 12:09:34 +0200

    add leetcode111

--- leetcode/leetcode111.py
+++ leetcode/leetcode111.py
@@ -15,3 +15,47 @@ class Solution:
             return max(l, r) + 1
 
         return min(l, r) + 1
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+
+        min_depth = 1 << 31 - 1
+        if root.left:
+            min_depth = min(min_depth, self.minDepth(root.left))
+        if root.right:
+            min_depth = min(min_depth, self.minDepth(root.right))
+
+        return min_depth + 1
+
+
+import collections
+
+
+class Solution:
+    def minDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        queue = collections.deque([root])
+        depth = 0
+        while queue:
+            depth += 1
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                if not node.left and not node.right:
+                    return depth
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return depth
