b0a027e31c9b0e5529847c173cb0f65370a7810a
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-01 12:09:11 +0200

    add leetcode102

--- leetcode/leetcode102.py
+++ leetcode/leetcode102.py
@@ -20,3 +20,31 @@ class Solution:
         res = self.BuildLevelOrder(root.right, res, level+1)
         return res
 
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+
+        while len(stack) > 0:
+            n = stack.pop()
+            if n:
+                if n.right:
+                    stack.append(n.right)
+                if n.left:
+                    stack.append(n.left)
+
+                stack.append(n)
+                stack.append(None)
+            else:
+                n = stack.pop()
+                res.append(n.val)
+        return res
