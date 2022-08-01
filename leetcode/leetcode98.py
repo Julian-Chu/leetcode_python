a1d26757b629b362f2cd2000062751de01aa1bf3
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-01 13:59:45 +0200

    update leetcode98

--- leetcode/leetcode98.py
+++ leetcode/leetcode98.py
@@ -5,6 +5,45 @@ from helper import TreeNode
 
 
 class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = [root]
+        prev = None
+
+        while stack:
+            node = stack.pop()
+
+            if node:
+                if node.right:
+                    stack.append(node.right)
+
+                stack.append(node)
+                stack.append(None)
+
+                if node.left:
+                    stack.append(node.left)
+            else:
+                node = stack.pop()
+                if not prev:
+                    prev = node
+                else:
+                    if prev.val >= node.val:
+                        return False
+                    prev = node
+        return True
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root, (-1 << 31) - 1, 1 << 31)
+
+    def helper(self, root: TreeNode, min_val: int, max_val: int) -> bool:
+        if not root:
+            return True
+
+        if root.val <= min_val or root.val >= max_val:
+            return False
+
+        return self.helper(root.left, min_val, root.val) and self.helper(root.right, root.val, max_val)
+
+class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
         if not root:
             return True
@@ -23,6 +62,7 @@ class Solution:
         if not self.isValidSubBST(root.right, root.val, upper):
             return False
         return True
+
 
 
 class TestSolution(TestCase):
