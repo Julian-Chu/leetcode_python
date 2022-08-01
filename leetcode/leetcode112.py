c43e5094a02eb96a9fea446913fdf2e55bb303ed
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-01 12:09:40 +0200

    add leetcode112

--- leetcode/leetcode112.py
+++ leetcode/leetcode112.py
@@ -19,3 +19,25 @@ class Solution:
             return False
         sum -= root.val
         return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+
+        def dfs(node: TreeNode, restSum: int) -> bool:
+            if not node:
+                return False
+
+            restSum -= node.val
+            if not node.left and not node.right:
+                return restSum == 0
+
+            return dfs(node.left, restSum) or dfs(node.right, restSum)
+
+        return dfs(root, targetSum)
