60ac7ad5642fad89cda28adadf993ef8ee0f4ff1
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-07 23:18:43 +0200

    add leetcode90

--- leetcode/leetcode90.py
+++ leetcode/leetcode90.py
@@ -14,3 +14,31 @@ class Solution:
         for i in range(idx, len(nums)):
             if i == idx or nums[i] != nums[i - 1]:
                 self.helper(res, nums, cur + [nums[i]], i + 1)
+
+
+class Solution:
+    def __init__(self):
+        self.res = []
+        self.tmp = []
+
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return []
+        self.res.clear()
+        self.tmp.clear()
+        nums.sort()
+        self.dfs(nums, 0)
+        return self.res
+
+    def dfs(self, nums: List[int], start_index: int):
+        self.res.append(self.tmp[:])
+        if start_index == len(nums):
+            return
+
+        for i in range(start_index, len(nums)):
+            if i > start_index and nums[i] == nums[i - 1]:
+                continue
+
+            self.tmp.append(nums[i])
+            self.dfs(nums, i + 1)
+            self.tmp.pop()
