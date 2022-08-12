2375f32745b9b309cecf5a24d4e23fe576f400f2
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-12 21:55:54 +0200

    update leetcode26

--- leetcode/leetcode26_RemoveDuplicatesFromSortedArray.py
+++ leetcode/leetcode26_RemoveDuplicatesFromSortedArray.py
@@ -1,7 +1,6 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        fast, slow = 0, 0
-
+        slow = 0
         for fast in range(len(nums)):
             if nums[fast] != nums[slow]:
                 slow += 1
