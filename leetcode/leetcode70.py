5345e1756e60456bc6e309fc90e3a8ab6f6fe6d5
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-21 11:38:15 +0200

    update leetcode70

--- leetcode/leetcode70.py
+++ leetcode/leetcode70.py
@@ -37,3 +37,35 @@ class TestSolution(TestCase):
         solution = Solution();
         for ts in test_cases:
             self.assertEqual(solution.climbStairs(ts["input"]), ts["expected"])
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+
+        steps = [0] * (n+1)
+
+        steps[0] = 1
+        steps[1] = 1
+
+        for i in range(2, n+1):
+            steps[i] = steps[i-1] + steps[i-2]
+
+        return steps[n]
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 1:
+            return 1
+
+        steps = [0] * (2)
+
+        steps[0] = 1
+        steps[1] = 1
+
+        for i in range(2, n+1):
+            steps[i%2] = steps[(i-1)%2] + steps[(i-2)%2]
+
+        return steps[n%2]
