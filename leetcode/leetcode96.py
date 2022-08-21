5534d3e26f372f6597db27fb47870354a8abcda8
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-21 11:38:24 +0200

    update leetcode96

--- leetcode/leetcode96.py
+++ leetcode/leetcode96.py
@@ -36,3 +36,16 @@ class TestSolution(TestCase):
         solution = Solution()
         for ts in test_cases:
             self.assertEqual(ts["expected"], solution.numTrees(ts["input"]))
+
+class Solution:
+    def numTrees(self, n: int) -> int:
+        if n <= 1:
+            return n
+        dp = [0] * (n+1)
+        dp[0] = 1 # no tree
+
+        for i in range(1, n+1):
+            for j in range(1, i+1):
+                dp[i] += dp[j-1] * dp[i-j]
+
+        return dp[n]
