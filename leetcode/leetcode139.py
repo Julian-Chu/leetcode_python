9754b0fd00814c2d66dee198f5569fec11cc4385
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-22 13:14:03 +0200

    add leetcode139

--- leetcode/leetcode139.py
+++ leetcode/leetcode139.py
@@ -47,3 +47,35 @@ class TestSolution(TestCase):
         for testcase in test_cases:
             self.assertEqual(solution.wordBreak(testcase["s"], testcase["dict"]), testcase["expected"],
                              testcase["name"])
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False] * (n+1)
+
+        dp[0] = True
+
+        for i in range(1, n+1):
+            for word in wordDict:
+                if i >= len(word):
+                    dp[i] = dp[i] or (dp[i-len(word)] and s[(i- len(word)):i] == word)
+
+        return dp[n]
+
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        def dfs(s: str, word_dict: set, start: int, memo: dict)->bool:
+            if start == len(s):
+                return True
+
+            if start in memo:
+                return memo[start]
+
+            for end in range(start+1, len(s)+1):
+                if s[start:end] in word_dict and dfs(s, word_dict, end, memo):
+                    memo[start] = True
+                    return memo[start]
+            memo[start] = False
+            return memo[start]
+
+        return dfs(s, set(wordDict), 0, {})
