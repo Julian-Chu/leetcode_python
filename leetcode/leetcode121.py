3a8c35e43fcc51cc3c29e7b250a3a287aa24dd39
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-22 13:13:43 +0200

    add leetcode121

--- leetcode/leetcode121.py
+++ leetcode/leetcode121.py
@@ -1,6 +1,36 @@
 from typing import List
 from unittest import TestCase
+# greedy
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
 
+        if not prices:
+            return 0
+
+        low = prices[0]
+        result = 0
+        for price in prices:
+            low = min(low, price)
+            result = max(result, price - low)
+
+        return result
+
+# dp
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        if not prices:
+            return 0
+
+        dp = [[0] * 2 for _ in range(len(prices))]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+
+        for i in range(1, len(prices)):
+            dp[i][0] = max(dp[i-1][0], - prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+
+        return dp[-1][1]
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
