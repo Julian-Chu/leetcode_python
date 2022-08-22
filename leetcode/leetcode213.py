e0eab716326c8aa26f715804b408fb5db23ff19e
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-22 13:14:20 +0200

    add leetcode213

--- leetcode/leetcode213.py
+++ leetcode/leetcode213.py
@@ -1,6 +1,34 @@
 from typing import List
 from unittest import TestCase
 
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        def robRange(nums_original, start, end) -> int:
+            nums = nums_original[start:end+1]
+            n = len(nums)
+
+            if n == 0:
+                return 0
+            if n == 1:
+                return nums[0]
+            dp = [0] * n
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+
+            for i in range(2, n):
+                dp[i] = max(dp[i-2] + nums[i], dp[i-1])
+
+            return dp[-1]
+
+
+        return max(robRange(nums, 0, len(nums)-2), robRange(nums, 1, len(nums)-1))
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
