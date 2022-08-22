9d16e5f7c1ea4ad37e4a71e7124206e0124ec74d
Yu-Lang Chu <yulang.chu@gmail.com>
2022-08-22 13:13:51 +0200

    add leetcode122

--- leetcode/leetcode122.py
+++ leetcode/leetcode122.py
@@ -1,6 +1,20 @@
 from typing import List
 from unittest import TestCase
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:
+            return 0
+        n = len(prices)
+
+        dp = [[0] * 2 for _ in range(n)]
+        dp[0][0] = -prices[0]
+        dp[0][1] = 0
+
+        for i in range(1, n):
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] - prices[i])
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] + prices[i])
+        return dp[-1][1]
 
 class Solution:
     def maxProfit(self, price: List[int]) -> int:
